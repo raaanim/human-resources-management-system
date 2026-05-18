f6c94ee87de0cb57feacbdee0fbcbdb3a38529fe
Ranim Ben Braham <benbrahamranim@gmail.com>
2026-05-18 11:28:52 +0200

    refactor: add service interfaces for Employee and auth controllers

--- src/main/java/aitho/ranim/hrms/controller/security/IAuthController.java
+++ src/main/java/aitho/ranim/hrms/controller/security/IAuthController.java
@@ -1,6 +1,12 @@
 package aitho.ranim.hrms.controller.security;
 
+import aitho.ranim.hrms.dto.security.LoginRequest;
+import aitho.ranim.hrms.dto.security.LoginResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public interface IAuthController {
 
-
+    ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request);
 }
