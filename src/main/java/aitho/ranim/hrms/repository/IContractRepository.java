a44504cfe5fab4be2799dd70b8bea70ad22feb33
Ranim Ben Braham <benbrahamranim@gmail.com>
2026-06-29 19:39:41 +0200

    feat: add scheduled contract expiry email notification

--- src/main/java/aitho/ranim/hrms/repository/IContractRepository.java
+++ src/main/java/aitho/ranim/hrms/repository/IContractRepository.java
@@ -5,12 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface IContractRepository extends JpaRepository<Contract, Long> {
     List<Contract> findByEmployeeIdOrderByStartDateDesc(Long employeeId);
     Optional<Contract> findFirstByEmployeeIdAndActiveTrue(Long employeeId);
+    List<Contract>findByActiveTrueAndEndDateBetween(LocalDate start, LocalDate end);
 
     @Modifying
     @Query("""
